61f2e87e4d28ffdf46dab4fc561a6e6b7a618103
jrusso1020 <jdr289@cornell.edu>
2017-03-13 20:28:26 -0400

    adding start date, end date, and url to conference model

--- Web/db/migrate/20170313193314_create_conferences.rb
+++ Web/db/migrate/20170313193314_create_conferences.rb
@@ -2,7 +2,9 @@ class CreateConferences < ActiveRecord::Migration[5.0]
   def change
     create_table :conferences do |t|
       t.string :name
-      t.datetime :date
+      t.datetime :start_date
+      t.datetime :end_date
+      t.text :url
       t.string :location
 
       t.timestamps
