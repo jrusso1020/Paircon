f95791937097a92f8026185146f43147b4ab2d51
Mutahir <kazmi.mutahir@gmail.com>
2017-03-15 16:36:57 -0400

    Sidekiq Start Bug Fixed

--- Web/config/initializers/sidekiq.rb
+++ Web/config/initializers/sidekiq.rb
@@ -2,10 +2,10 @@ redis_host = ENV['REDIS_HOST'] || 'localhost'
 redis_port = ENV['REDIS_PORT'] || '6379'
 Sidekiq.configure_server do |config|
   config.average_scheduled_poll_interval = 15
-  config.redis = { :url => "redis://#{redis_host}:#{redis_port}/12", :namespace => "paircon_#{Rails.env}" }
+  config.redis = { :url => "redis://#{redis_host}:#{redis_port}/12" }
 end
 
 # When in Unicorn, this block needs to go in unicorn's `after_fork` callback:
 Sidekiq.configure_client do |config|
-  config.redis = { :url => "redis://#{redis_host}:#{redis_port}/12", :namespace => "paircon_#{Rails.env}" }
+  config.redis = { :url => "redis://#{redis_host}:#{redis_port}/12" }
 end
