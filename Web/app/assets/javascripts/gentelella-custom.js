a9d18e528e94232b7665b123037eb671332401d6
Mutahir <kazmi.mutahir@gmail.com>
2017-03-24 13:04:57 -0400

    User Settings Fixed

--- Web/app/assets/javascripts/gentelella-custom.js
+++ Web/app/assets/javascripts/gentelella-custom.js
@@ -4871,7 +4871,7 @@ $(document).ready(function() {
 
     // init_sparklines();
     // init_flot_chart();
-    init_sidebar();
+    // init_sidebar();
     // init_wysiwyg();
     // init_InputMask();
     // init_JQVmap();
