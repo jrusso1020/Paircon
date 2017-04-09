62c8a843a788559d1567ec95f2024541abb8fcff
Mahak Garg <mahakgrg@gmail.com>
2017-04-09 19:13:41 -0400

    Paper addition

---
+++ Web/app/controllers/papers_controller.rb
@@ -0,0 +1,79 @@
+class PapersController < ApplicationController
+  include ConferencesHelper
+  before_action :authenticate_user!, except: [:validation]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @title = 'Add Paper'
+    @body = 'Please enter the paper title and file upload path'
+    render layout: false
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+
+  def paper_params
+    uploaded_io = params[:paper][:file]
+    pdf_path = Rails.root.join('public', 'conference', params[:conference_id], 'pdf', uploaded_io.original_filename)
+    pdf_folder = Rails.root.join('public', 'conference', params[:conference_id], 'pdf')
+    FileUtils::mkdir_p pdf_folder
+    File.open(pdf_path, 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    scrapper = PDFScrapper.new("dummy", PDFScrapper::PageType[:personal])
+    txt_file = Rails.root.join('public', 'conference', params[:conference_id], 'txt', File.basename(uploaded_io.original_filename, File.extname(uploaded_io.original_filename)) + ".txt")
+    txt_folder = Rails.root.join('public', 'conference', params[:conference_id], 'txt')
+    FileUtils::mkdir_p txt_folder
+    scrapper.convertSinglePdfToText(pdf_path, txt_folder)
+    paper_params = {
+        "title" => params[:paper][:title],
+        "path" => txt_file,
+        "pdf_link" => pdf_path
+    }
+    paper_params
+  end
+
+  def create
+    @paper = Paper.new(paper_params)
+    if @paper.save
+      @conferencePaper = ConferencePaper.new(:conference_id => params[:conference_id], :paper_id => @paper.id)
+      if @conferencePaper.save
+        flash[:notice] = "Successfully created paper!"
+      else
+        flash[:alert] = "Error creating new paper!"
+      end
+    else
+      flash[:alert] = "Error creating new paper!"
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+
+  def index
+    @is_organizer = is_organizer(params[:conference_id], current_user)
+    @papers = papers_by_conference params[:conference_id]
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def delete
+    render layout: false
+  end
+
+  # The destroy action removes the conference permanently from the database
+  def destroy
+    Paper.transaction do
+      if @paper.destroy and @conferencePaper.destroy
+        flash[:notice] = 'Paper has been successfully deleted.'
+      else
+        flash[:error] = 'Unable to delete Paper due to some error. Please try again later ...'
+      end
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+end
--- Web/app/helpers/conferences_helper.rb
+++ Web/app/helpers/conferences_helper.rb
@@ -18,4 +18,9 @@ module ConferencesHelper
   def is_organizer conference_id, user
     Conference.find_by_id(conference_id).conference_organizers.exists?(user_id: user.id) and !user.attendee?
   end
+
+  def papers_by_conference conference_id
+    Paper.includes(:conference_papers).where(conference_papers: {conference_id: conference_id}).order(:created_at)
+  end
+
 end
