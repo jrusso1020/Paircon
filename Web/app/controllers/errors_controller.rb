51508bcac7b8299ed25ce0102a582156da5cebf8
Mutahir Kazmi <kazmi.mutahir@gmail.com>
2017-04-11 11:20:05 -0400

    [Paircon - 26] Public View Complete (#87)
    * Public View Complete
    
    * Bug Fix
    
    * Merge Bug Fix
    
    * Sign up fixed

--- Web/app/controllers/errors_controller.rb
+++ Web/app/controllers/errors_controller.rb
@@ -1,13 +1,13 @@
 class ErrorsController < ApplicationController
   def not_found
-    render status: 404
+    render status: 404, layout: signed_in?(:user) ? 'application' : 'error'
   end
 
   def internal_server_error
-    render status: 500
+    render status: 500, layout: signed_in?(:user) ? 'application' : 'error'
   end
 
   def unauthorized_access
-    render status: 403
+    render status: 403, layout: signed_in?(:user) ? 'application' : 'error'
   end
 end
