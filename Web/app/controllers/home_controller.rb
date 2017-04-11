475744d90ac770e6683b4931fc180361c1e0f689
justinachen <jjc382@cornell.edu>
2017-04-11 11:35:42 -0400

    updating partial views for home dashboard. notification section incomplete

--- Web/app/controllers/home_controller.rb
+++ Web/app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    
+    # notification section
+      @notifications = Notification.find_all_notifications(current_user, Notification::NOTIFICATION_LIST_LIMIT)
+      @total_notifications = Notification.find_new_notifications(@notifications, current_user)
+      # render :partial => 'notifications/notifications_listing'
   end
 
   def privacy_policy
