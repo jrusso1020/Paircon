2ae32e83686d02c88f79f0d2cc36e6886d29bf3b
Sahana <stp66@cornell.edu>
2017-04-12 00:33:08 -0400

    Events in progress

--- Web/app/controllers/home_controller.rb
+++ Web/app/controllers/home_controller.rb
@@ -3,12 +3,12 @@ class HomeController < ApplicationController
 
   def index
     # notification section
-
+      authenticate_user!
       @notifications = Notification.find_all_notifications(current_user, Notification::NOTIFICATION_LIST_LIMIT)
       @total_notifications = Notification.find_new_notifications(@notifications, current_user)
       @conferences_attendee_list = Conference.my_attending_conferences_active(current_user)
       @conferences_organizer_list = Conference.my_organizing_conferences_active(current_user)
-
+     @user_id = current_user.id
 
   end
 
@@ -19,5 +19,7 @@ class HomeController < ApplicationController
   def terms
     render layout: false
   end
+
+
 
 end
