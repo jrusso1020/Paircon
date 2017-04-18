96c14cb9ff494efea54ed9e4bf3798b4917b790b
justinachen <jjc382@cornell.edu>
2017-04-18 16:28:24 -0400

    fixing schema conflict and re-commenting rec modal css

--- Web/db/schema.rb
+++ Web/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170413002130) do
-=======
-ActiveRecord::Schema.define(version: 20170408035141) do
->>>>>>> signup_layout
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
