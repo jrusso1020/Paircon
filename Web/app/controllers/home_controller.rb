2c452fa22300d1d8814576a9ffe36db52eb59897
YaoChuanChang <yc2265@cornell.edu>
2017-04-30 19:55:34 -0400

    squashed introjs

--- Web/app/controllers/home_controller.rb
+++ Web/app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
+
   before_action :authenticate_user!, only: [:home, :search]
 
   def index
+      authenticate_user!
+      @notifications = Notification.find_all_notifications(current_user, Notification::NOTIFICATION_LIST_LIMIT)
+      @total_notifications = Notification.find_new_notifications(@notifications, current_user)
+      @conferences_attendee_list = Conference.my_attending_conferences_active(current_user)
+      @conferences_organizer_list = Conference.my_organizing_conferences_active(current_user)
+     @user_id = current_user.id
+
   end
 
   def privacy_policy
@@ -12,6 +20,7 @@ class HomeController < ApplicationController
     render layout: false
   end
 
+
   def search
     query = params[:search_val]
     @conferences = []
@@ -21,5 +30,6 @@ class HomeController < ApplicationController
 
     render :search_results
   end
+
 
 end
