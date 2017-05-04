f2a7b4b338543f460279f165e31ed21da7b83aa1
stp66 <stp66@cornell.edu>
2017-05-03 23:37:29 -0400

    [Paircon - 46]  Datatable fix (#125)
    * Fixing datatable bug
    
    * adding moment format for all datatables
    
    * deleted files
    
    * adding changes suggested
    
    * adding conference papers in gitignore
    
    * modifying gitignore
    
    * deleting txts
    
    * adding options
    
    * Adding changes suggested
    
    * making email sortable.
    
    * changes for search results table
    
    * changing style pending organizer table

--- Web/db/schema.rb
+++ Web/db/schema.rb
@@ -205,20 +205,9 @@ ActiveRecord::Schema.define(version: 20170501180806) do
     t.string   "user_industry",                      default: ""
     t.integer  "user_grad_year"
     t.string   "user_organization",                  default: ""
-    t.string   "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer  "invitation_limit"
-    t.string   "invited_by_type"
-    t.integer  "invited_by_id"
-    t.integer  "invitations_count",                  default: 0
     t.boolean  "is_scraped",                         default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
-    t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
-    t.index ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
