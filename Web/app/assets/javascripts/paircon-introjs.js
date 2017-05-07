fbe37ffb37d064b6536eb59b223a6c7952ec616e
YaoChuanChang <yc2265@cornell.edu>
2017-05-06 21:18:20 -0400

    fixed  backBtn bug

--- Web/app/assets/javascripts/paircon-introjs.js
+++ Web/app/assets/javascripts/paircon-introjs.js
@@ -16,10 +16,12 @@ function initializeIntroJS(id) {
     if (Cookies.get("introjs_checked") == "true") {
         if (typeof Cookies.get(id + '_introjs') === 'undefined') {
             Cookies.set(id + '_introjs', true);
+            Cookies.set(id + '_introjs_prev_step', false);
         }   
     }
     if (Cookies.get("introjs_checked") == "false") {
         Cookies.set(id + '_introjs', false);
+        Cookies.set(id + '_introjs_prev_step', false);
     }
     startIntroJS(id);
 }
@@ -28,27 +30,46 @@ function startIntroJS(id) {
     $('#startButton').click(function () {
         Cookies.set("introjs_checked", true);
         Cookies.set(id + '_introjs', true);
-        nextIntroJS(id, 1, 'posts');
+        Cookies.set(id + '_introjs_prev_step', false);
+        nextIntroJS(id, 1, 1, 'home', 'posts');
     });
 }
 
-function nextIntroJS(id, nextStep, nextPage) {
+function nextIntroJS(id, prevStep, nextStep, prevPage, nextPage) {
     if (Cookies.get(id + '_introjs') == "true") {
         if (nextPage == "home") {
             Cookies.set("introjs_checked", false);
             Cookies.set(id + '_introjs', false);     
         } 
-        introJs().setOption('doneLabel', 'Next page').goToStepNumber(nextStep).start().oncomplete(function() {
-            $("#" + nextPage).click();
-        });  
+        if (Cookies.get(id + '_introjs_prev_step') == "false") {
+            introJs().setOption('doneLabel', 'Next page').goToStepNumber(nextStep).start().oncomplete(function() {
+                $("#" + nextPage).click();
+            });  
+        } else if (Cookies.get(id + '_introjs_prev_step') == "true") {
+            Cookies.set(id + '_introjs_prev_step', false);
+            introJs().setOption('doneLabel', 'Next page').goToStepNumber(prevStep).start().oncomplete(function() {
+                $("#" + nextPage).click();
+            });  
+        }
     } else {
         introJs().exit();
     }
 
-    $('.introjs-nextbutton').click(function () {
+    $('.introjs-prevbutton').click(function () {
+        Cookies.set(id + '_introjs_prev_step', true);
         var xStr = document.querySelector( ".introjs-helperNumberLayer" );
         var xInt = parseInt(xStr.innerText)
+        if (nextStep == xInt) {
+            introJs().exit();
+            $("#" + prevPage).click();
+        }
+    });
+
+    $('.introjs-nextbutton').click(function () {
+        var xStr = document.querySelector( ".introjs-helperNumberLayer" );
+        var xInt = parseInt(xStr.innerText);
         if (xInt >= 14) {
+            introJs().exit();
             $("#" + nextPage).click();
         }
     }); 
