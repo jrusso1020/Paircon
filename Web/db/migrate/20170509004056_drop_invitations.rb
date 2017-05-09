19d886828168b9ff78292a78d1b1951e7432ed71
YaoChuanChang <yc2265@cornell.edu>
2017-05-08 21:49:04 -0400

    Fixed Error Migration

--- Web/db/migrate/20170509004056_drop_invitations.rb
+++ Web/db/migrate/20170509004056_drop_invitations.rb
@@ -2,13 +2,16 @@ class DropInvitations < ActiveRecord::Migration[5.0]
   disable_ddl_transaction!
   
   def change
-    remove_column :users, :invitation_token
-    remove_column :users, :invitation_created_at
-    remove_column :users, :invitation_sent_at
-    remove_column :users, :invitation_accepted_at
-    remove_column :users, :invitation_limit
-    remove_column :users, :invited_by_type
-    remove_column :users, :invited_by_id
-    remove_column :users, :invitations_count
+    begin
+	remove_column :users, :invitation_token
+    	remove_column :users, :invitation_created_at
+    	remove_column :users, :invitation_sent_at
+    	remove_column :users, :invitation_accepted_at
+    	remove_column :users, :invitation_limit
+    	remove_column :users, :invited_by_type
+    	remove_column :users, :invited_by_id
+   	remove_column :users, :invitations_count
+    rescue
+    end
   end
 end
